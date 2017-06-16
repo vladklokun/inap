329177138f16d3ef5ec28ed9d56a5b4360550237
tempname <vklokun@protonmail.ch>
2017-06-16 23:05:02 +0300

    Preliminary support for getting entropy from /dev/urandom

--- src/sysrand.c
+++ src/sysrand.c
@@ -1,6 +1,51 @@
 #include "sysrand.h"
 
-uint32_t sysrand(void)
+#if !defined(HAVE_GETRANDOM) && !defined(HAVE_SAFE_ARC4RANDOM)
+
+# ifndef SSIZE_MAX
+#  define SSIZE_MAX (SIZE_MAX/2 -1)
+# endif
+/*
+ * This global variable is a file descriptor for urandom.
+ */
+static int urandomfd;
+
+int
+sysranddevopen(void)
+{
+	urandomfd = open("/dev/urandom", O_RDONLY);
+
+	return urandomfd;
+}
+
+int
+sysranddevclose(void)
+{
+	return close(urandomfd);
+}
+
+/* 
+ * This function fills buffer buf with buflen bytes from /dev/urandom. Intended
+ * for any OS that doesn't have an entropy supplying syscall (e.g macOS).
+ * Assumes POSIX environment.
+ */
+ssize_t
+getdevrandom(const int fd, void *buf, size_t buflen)
+{
+	ssize_t readbcnt = 0;
+
+	assert(buflen > (ssize_t) 0U);
+	assert(buflen < SSIZE_MAX);
+
+	while ((readbcnt = read(fd, buf, buflen)) < (ssize_t) 0 ||
+	        (errno == EAGAIN || errno == EINTR));
+
+	return readbcnt;
+}
+#endif
+
+uint32_t
+sysrand(void)
 {
 	uint32_t r = 0;
 	int readb = 0;
@@ -16,7 +61,11 @@ uint32_t sysrand(void)
 	return r;
 }
 
-uint32_t sysrandu(uint32_t up)
+/*
+ * This function returns a uint32_t which is uniformly distributed on [0, up).
+ */
+uint32_t
+sysrandu(uint32_t up)
 {
 	uint32_t min = 0;
 	uint32_t r = 0;
--- src/sysrand.h
+++ src/sysrand.h
@@ -4,5 +4,18 @@
 #include <assert.h>
 #include <sys/syscall.h>
 
+#if !defined(HAVE_GETRANDOM)
+# include <sys/stat.h> /* open() */
+# include <fcntl.h> /* open() */
+
+# include <limits.h> /* SSIZE_MAX */
+#endif
+
+#if !defined(HAVE_GETRANDOM)
+int sysranddevopen(void);
+int sysranddevclose(void);
+ssize_t getdevrandom(const int fd, void *buf, size_t buflen);
+#endif
+
 uint32_t sysrand(void);
 uint32_t sysrandu(uint32_t up);
