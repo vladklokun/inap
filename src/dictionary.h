37f02553d6a890735f4a23847880e4006351923d
tempname <vklokun@protonmail.ch>
2017-06-17 17:16:43 +0300

    Removed include guards in dictionary.h.

--- src/dictionary.h
+++ src/dictionary.h
@@ -1,17 +1,6 @@
-#ifndef	STDIO_H
-#define	STDIO_H
 #include <stdio.h>
-#endif
-
-#ifndef STDLIB_H
-#define STDLIB_H
 #include <stdlib.h>
-#endif
-
-#ifndef STRING_H
-#define STRING_H
 #include <string.h>
-#endif
 
 struct Dictionary {
 	/* dynamically allocated array of c-strings */
