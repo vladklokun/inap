8680db4cb42871a1c56059b40efc7fb813461937
tempname <vklokun@protonmail.ch>
2017-06-17 17:18:52 +0300

    Added desired cmd argument parsing.

--- src/main.c
+++ src/main.c
@@ -4,12 +4,34 @@
 
 #include "password.h"
 
+void usage(void);
+
+void usage(void)
+{
+	fputs("Usage: inap [ OPTIONS ] [ elnum ] [ pwnum ]\n\n", stderr);
+
+	fputs("Options:\n", stderr);
+	fputs("-d /path/to/dictionary\n", stderr);
+	fputs("\tUse given path as a dictionary file.\n", stderr);
+
+	fputs("-l {en, ru}\n", stderr);
+	fputs("\tUse predefined dictionary of specified language.\n", stderr);
+
+	fputs("elnum\n", stderr);
+	fputs("\tNumber of elements that comprise a password.\n", stderr);
+
+	fputs("pwnum\n", stderr);
+	fputs("\tNumber of passwords to generate.\n", stderr);
+
+}
+
 int main(int argc, char *argv[])
 {
-	uint16_t wordnum = 6;
+	uint16_t elnum = 6;
 	uint16_t pwnum = 1;
 	const char *dictpath = "dict/english";
 	const char *sep = " ";
+	char *tmp;
 
 	struct Dictionary *dp = NULL;
 	struct Password *pp = NULL;
@@ -18,25 +40,44 @@ int main(int argc, char *argv[])
 
 	int c;
 
-	while ((c = getopt(argc, argv, "d:l:")) != -1 ) {
+	while ((c = getopt(argc, argv, "d:hl:")) != -1 ) {
 		switch (c) {
 		case 'd':
 			dictpath = optarg;
 			break;
-		case '?':
-			if (optopt == 'd') {
-				fprintf(stderr,
-					"Option -%c requires an argument\n",
-					optopt);
-			} else if (isprint(optopt)) {
-				fprintf(stderr, "Unknown option -%c\n", optopt);
+		case 'l':
+			if (strncmp(optarg, "en", 2) == 0) {
+				/* dictpath stays as english */
+			} else if (strncmp(optarg, "ru", 2) == 0) {
+				dictpath = "dict/russian";
 			} else {
-				fprintf(stderr,
-					"Unknown option char `\\x%x`", optopt);
+				fputs("unsupported dictionary\n", stderr);
+				return 1;
 			}
+			break;
+		case 'h':
+		case '?':
+			usage();
 			return 1;
 		default:
 			abort();
+		}
+	}
+
+	if (optind < argc) {
+		elnum = strtol(argv[optind], &tmp, 0);
+		if (*tmp) {
+			fputs("Invalid number of passwords", stderr);
+			return 1;
+		}
+		optind++;
+	}
+
+	if (optind < argc) {
+		pwnum = strtol(argv[optind], &tmp, 0);
+		if (*tmp) {
+			fputs("Invalid number of elements in password", stderr);
+			return 1;
 		}
 	}
 
@@ -67,7 +108,7 @@ int main(int argc, char *argv[])
 	for (uint16_t i = 0; i < pwnum; i++) {
 		pp = passinit(pp);
 		
-		passgen(pp, dp, wordnum, sep);
+		passgen(pp, dp, elnum, sep);
 		passprint(pp);
 		
 		passfree(pp);
